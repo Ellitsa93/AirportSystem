0d3b1eefa68f76406053ea9b86badb2a6940a023
Ellitsa93 <ellahr93@gmail.com>
2015-06-08 17:44:54 +0300

    Create account_manager.py

---
+++ account_manager.py
@@ -0,0 +1,22 @@
+import rich_client
+import administrator
+import passenger
+
+
+def main():
+    pass
+
+
+def check_category(username, password):
+    pass
+
+
+def registration():
+    pass
+
+
+def check_administrator_access(name):
+    pass
+
+if __name__ == '__main__':
+    main()

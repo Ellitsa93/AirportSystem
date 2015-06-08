07a3c31405a4bafab27ec44a0e36f92d39a7bfff
Ellitsa93 <ellahr93@gmail.com>
2015-06-08 17:41:51 +0300

    Create passenger.py

---
+++ passenger.py
@@ -0,0 +1,13 @@
+import user
+
+
+class Passenger (User):
+
+    def send_ticket(self, ticket_id):
+        pass
+
+    def propose_destination(self):
+        pass
+
+    def start_destination_quiz(self):
+        pass

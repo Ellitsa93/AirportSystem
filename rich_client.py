4725c2202102925a290d88b5e2491a3e0bf79f9a
Ellitsa93 <ellahr93@gmail.com>
2015-06-08 17:43:02 +0300

    Create rich_client.py

---
+++ rich_client.py
@@ -0,0 +1,14 @@
+import user
+import rich_people_calendar
+
+
+class RichClient (User):
+
+    def send_permission(self, date):
+        pass
+
+    def propose_rich_people_for_appointment(self):
+        pass
+
+    def make_appointment(self, name):
+        pass

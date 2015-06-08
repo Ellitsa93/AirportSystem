defa9372f00e6f28e39ded2ef15d6d09dd5186ba
Ellitsa93 <ellahr93@gmail.com>
2015-06-08 17:42:15 +0300

    Create administrator.py

---
+++ administrator.py
@@ -0,0 +1,13 @@
+import user
+
+
+class Administrator (User):
+
+    def check_passenger(self, ticket_number, name, flight_id, seat):
+        pass
+
+    def check_for_free_private_runway(self):
+        pass
+
+    def give_permission(self):
+        pass
